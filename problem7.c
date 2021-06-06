a9c390f3d9e66be15eccbed16c13dce87ef88717
Benjamin Barona <benjibar01@hotmail.com>
2021-06-05 21:59:03 -0500

    first commit

---
+++ problem7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+float a, b, c, i = 500;
+
+for (c = 1; c < i; c++){
+
+	for (b = 1; b < c; b++){
+
+		for (a = 1; a < b; a++){
+
+float d = ((a * a)+(b * b))/(c * c);
+
+if (d/1 == 1){
+
+
+printf("%.0f %.0f %.0f\n", a, b, c);}
+
+
+
+				       }
+			       }
+			}
+return 0;
+	}
+
+
+
+
