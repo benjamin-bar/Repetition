1cc95a0a4648d06ee082213b5301c440add198f3
Benjamin Barona <benjibar01@hotmail.com>
2021-06-09 18:04:00 -0500

    first commit

---
+++ problem9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+
+int a, i;
+
+scanf("%d", &a);
+
+while ( a % 2 == 0){
+
+printf("2 ");
+a = a/2;
+}
+
+for (i = 3; i <= sqrt(a); i = i+2){
+
+while ( a % i == 0){
+
+printf("%d ", i);
+
+a = a/i;
+}
+}
+if (a > 2){
+printf ("%d ", a);}
+
+return 0;
+}
