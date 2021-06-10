9bd77d3eba924f49c31361a56ce1ad57f53f2810
Benjamin Barona <benjibar01@hotmail.com>
2021-06-10 17:42:04 -0500

    second commit

--- problem13.c
+++ problem13.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b, i;
+int main(){
 
-    scanf("%d", &a);
+int a, b, i;
 
+scanf("%d", &a);
 
-    for(i = a; i >= 1; i--)
-    {
-        for(b = a; b > i; b--)
-        {
-            printf("%d", b);
-        }
+for(i = a; i >= 1; i--){
 
-        for(b = 1; b <= (i * 2 - 1); b++)
-        {
-            printf("%d", i);
-        }
+for(b = a; b > i; b--){
 
-        for(b = i + 1; b <= a; b++)
-        {
-            printf("%d", b);
-        }
+printf("%d", b);
+}
 
-        printf("\n");
-    }
+for(b = 1; b <= (i * 2 - 1); b++){
 
-    for(i = 1; i < a; i++)
-    {
+printf("%d", i);
+}
 
-        for(b = a; b > i; b--)
-        {
-            printf("%d", b);
-        }
+for(b = i + 1; b <= a; b++){
 
-        for(b = 1; b <= (i * 2 - 1); b++)
-        {
-            printf("%d", i+1);
-        }
+printf("%d", b);
+}
 
-        for(b = i + 1 ; b <= a; b++)
-        {
-            printf("%d", b);
-        }
+printf("\n");
+}
 
-        printf("\n");
-    }
+for(i = 1; i < a; i++){
 
-    return 0;
+for(b = a; b > i; b--){
+
+printf("%d", b);
+}
+
+for(b = 1; b <= (i * 2 - 1); b++){
+
+printf("%d", i+1);
+}
+
+for(b = i + 1 ; b <= a; b++){
+
+printf("%d", b);
+}
+
+printf("\n");
+}
+
+return 0;
 }
